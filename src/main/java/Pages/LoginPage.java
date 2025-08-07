a3c2007505ecb49563702db3df01232023e83652
Ramnath <ramchinna058@gmail.com>
2025-08-07 16:10:57 +0530

    commit

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -11,19 +11,35 @@ public class LoginPage {
 	 public LoginPage(WebDriver driver) {
 		 this.driver =driver;
 	 }
-	 
-	 public void Login() {
+	 //Validating Loginpage
+	 public void loginpageValidation() {
 		 WaitUtilities wait = new WaitUtilities(driver);
-		 WebElement Username = driver.findElement(By.xpath("//input[@placeholder='Username']"));
-		 WebElement Password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+		 WebElement UsernameInput = driver.findElement(By.xpath("//input[@placeholder='Username']"));
+		 WebElement PasswordInput = driver.findElement(By.xpath("//input[@placeholder='Password']"));
 		 WebElement Login = driver.findElement(By.xpath("//button[@type='submit']"));
+		 WebElement Header = driver.findElement(By.xpath("//h5[text()='Login']"));
+		 WebElement OrangeLogo = driver.findElement(By.xpath("//img[@alt='company-branding']"));
+		 WebElement Usenamelabelpara = driver.findElement(By.xpath("//p[contains(.,'Admin')]"));
+		 WebElement Passwordlabelpara = driver.findElement(By.xpath("//p[contains(.,'admin123')]"));
+		 WebElement LinkText = driver.findElement(By.xpath("//p[contains(.,'Forgot your')]"));
+		 wait.Eleemnttobevisible(driver, Header, 10);
+		 System.out.println(OrangeLogo.isDisplayed());
+		 System.out.println(Usenamelabelpara.isDisplayed());
+		 System.out.println(Passwordlabelpara.isDisplayed());
+		 System.out.println(LinkText.isDisplayed());
+		 System.out.println(UsernameInput.isEnabled());
+		 System.out.println(PasswordInput.isEnabled());
+		 
+	 }
+	 public void Login(String Username,String Password) {
+		 WebElement UsernameInput = driver.findElement(By.xpath("//input[@placeholder='Username']"));
+		 WebElement PasswordInput = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+		 WebElement Login = driver.findElement(By.xpath("//button[@type='submit']"));
+		 WaitUtilities wait = new WaitUtilities(driver);
 		 wait.Eleemnttobevisible(driver, Login, 5);
-		 
-		 Username.sendKeys("Admin");
-		 Password.sendKeys("admin123");
+		 UsernameInput.sendKeys(Username);
+		 PasswordInput.sendKeys(Password);
 		 Login.click();
-		 
-		 
 		 
 	 }
 }
--- src/test/java/Test/LoginPageValidation.java
+++ src/test/java/Test/LoginPageValidation.java
@@ -1,15 +1,27 @@
 package Test;
 import Pages.LoginPage;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import BaseTest.BaseTest;
 
 public class LoginPageValidation extends BaseTest{
 	
+	
 
 	@Test
-	public void LoginTest() {
+	@Parameters({"username", "Password"})
+	public void LoginTest(@Optional("Admin")String Username, @Optional("admin123") String Password) {
 		LoginPage page = new LoginPage(driver);
-		page.Login();
+		page.Login(Username, Password);
 	}
+	
+	@Test
+	public void PageValidation() {
+		LoginPage page = new LoginPage(driver);
+		page.loginpageValidation();
+	}
+	
 }
